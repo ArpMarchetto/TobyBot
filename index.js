fd713702d78de80136918226dd8e8262eb75c691
ArpMarchetto <51139879+ArpMarchetto@users.noreply.github.com>
2019-05-28 20:26:26 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+var TelegramBot = require( 'node-telegram-bot-api' ),
+    util = require('util'),
+    express = require('express'),
+    app     = express(),
+    schedule = require('node-schedule');
+
+var TOKEN = process.env.TELEGRAM_API;
+var rule = new schedule.RecurrenceRule();
+var bot = new TelegramBot( TOKEN, { polling: true } );
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
