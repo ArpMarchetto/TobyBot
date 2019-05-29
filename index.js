13027c41667d5c3b53f9b628b735b800b9422f75
ArpMarchetto <51139879+ArpMarchetto@users.noreply.github.com>
2019-05-29 12:14:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function(request, response) {
 
 bot.on('message', (msg) => {
      //O comando acima diz o que o bot faz quando recebe mensagens
-	var Oi = ["oi","olá","ola", "oie"];
+	var Oi = ["oi","olá","ola","oie"];
 	var x = Math.random()*100+1;
 	
 	if (msg.text.toString().toLowerCase().indexOf(Oi[0]){
