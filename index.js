841960e83f900620d080927a3f9976b25303c0c0
ArpMarchetto <51139879+ArpMarchetto@users.noreply.github.com>
2019-05-29 10:51:58 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var TOKEN = process.env.TELEGRAM_API;
 var rule = new schedule.RecurrenceRule();
 var bot = new TelegramBot( TOKEN, { polling: true } );
 
-app.set('port', (process.env.PORT || 5000));
 //For avoiding Heroku $PORT error
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function(request, response) {
     var result = 'App is running'
     response.send(result);
@@ -36,9 +36,9 @@ bot.on('message', (msg) => {
 		return; }
 	
 	if(msg.text.toString().toLowerCase().indexOf(GoT) === 0) {
-		var vetor = ['Amo a Daenerys, traçava ela todinha','Eu só gosto de Game of Thrones pq os nórdicos são muito macho. Dá até orgulho de ver uma geração dessa',
+	var vetor = ['Amo a Daenerys, traçava ela todinha','Eu só gosto de Game of Thrones pq os nórdicos são muito macho. Dá até orgulho de ver uma geração dessa',
 			    'Casa Lannister é coisa de viado, vocês sabem, né?','Game of Thrones acabou do jeitinho que tinha que acabar', 'Adorei esse final','Eu gosto da série da HBO pq tem tudo que um homem pode gostar: violência e peitos']
-		bot.sendMessage(vetor[random1])
+				{bot.sendMessage(msg.chat.id, vetor[random1])}
 		return;	}
 
 	if(msg.text.toString().includes('homofobia'){
