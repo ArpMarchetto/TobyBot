ce8798b9f6ab6d81145016cc0c1234771866b42b
ArpMarchetto <51139879+ArpMarchetto@users.noreply.github.com>
2019-05-29 12:53:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,13 +20,33 @@ app.get('/', function(request, response) {
 
 bot.on('message', (msg) => {
      //O comando acima diz o que o bot faz quando recebe mensagens
-	var Oi = ["oi","olá","ola","oie"];
+	var Oi = ["oi","olá","ola","oie", "e aí", "e ai"];
+	var Tchau = ["falou", "flw", "falow", "tchau", "adeus"];
+	var GoT = ["Daenerys", "GoT", "Game of Thrones", "Jon Snow"];
 	var x = Math.random()*100+1;
-	for (var i = 0; i < Oi.length; i++) 
+	for (var i = 0; i < Oi.length; i++)
+	for (var o = 0; o < Oi.length; o++)
+	for (var u = 0; u < Oi.length; u++)	
 	
 	if (msg.text.toString().toLowerCase().indexOf(Oi[i])){
 		if (x <50) {bot.sendMessage(msg.chat.id,"Fala meu leitão véio")}
 		if (x >=50) {bot.sendMessage(msg.chat.id, "Coé, chapa")}
 		return;
-	} 
+	}
+	
+	if (msg.text.toString().toLowerCase().indexOf(Tchau[o])){
+		if (x <50) {bot.sendMessage(msg.chat.id,"Até a próxima, pela saco")}
+		if (x >=50) {bot.sendMessage(msg.chat.id, "Vá curtir a praia de Copacabana, manin. Sem Kaô")}
+		return;
+	}
+	
+	if (msg.text.toString().toLowerCase().indexOf(GoT[u])){
+		var vetorGoT = ['Amo a Daenerys, traçava ela todinha','Eu só gosto de Game of Thrones pq os nórdicos são muito macho. Dá até orgulho de ver uma geração dessa',
+			    'Casa Lannister é coisa de viado, vocês sabem, né?','Game of Thrones acabou do jeitinho que tinha que acabar', 
+				'Adorei esse final','Eu gosto da série da HBO pq tem tudo que um homem pode gostar: violência e peitos']
+				{bot.sendMessage(msg.chat.id, vetorGoT[random1])}
+		return;	
+	}
+	
+	
 });
