35a1418eb1b01d27e0deada6db5fa1600d1b0d31
ArpMarchetto <51139879+ArpMarchetto@users.noreply.github.com>
2019-05-28 21:27:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '862725699:AAEi2jR_obihgZByOGYEBQ4VBDfC0bFvpRE';
 const bot = new TelegramBot(token, {polling: true});
 
+
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+app.set('port', (process.env.PORT || 5000));
+
 bot.on('message', (msg) => {
      //O comando acima diz que o bot recebe mensagens
 	 var Hi = "hi";
