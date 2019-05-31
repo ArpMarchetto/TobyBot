43dbb9a9125e5703ed11cb3d48dbb55d5356171c
ArpMarchetto <51139879+ArpMarchetto@users.noreply.github.com>
2019-05-31 10:54:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var TelegramBot = require( 'node-telegram-bot-api' ),
 
 var TOKEN = process.env.TELEGRAM_API; //Fazer TELEGRAM_API como uma VAR no Heroku.
 var rule = new schedule.RecurrenceRule();
-var bot = new TelegramBot( TOKEN, { polling: true } );
+const bot = new TelegramBot( TOKEN, { polling: true } );
 
 //For avoiding Heroku $PORT error
 app.set('port', (process.env.PORT || 5000));
@@ -30,13 +30,13 @@ bot.on('message', (msg) => {     //O comando acima diz o que o bot faz quando re
             bot.inlineButton('Site e edições', {url: 'https://mafagaforevista.com.br/'}),
             bot.inlineButton('Twitter', {url: "https://twitter.com/mafagaforevista"})
         ], 
-		[
+	[
             bot.inlineButton('Quero Publicar', {msg: 'Antes de tudo, é bom avisar que a Mafagafo publica contos e noveletas, o que significa que sua história precisa ter de 7,5 a 17,5 mil palavras. É o que tem a sua história? Ótimo! Então o próximo passo é verificar as datas e preparar o envio. Nessa etapa, você vai enviar o começo da sua hitória, separando um trecho entre 2 e 3 mil palavras. Você também vai precisar enviar a estimativa do número de palavras no texto final, o conceito bem resumido da sua história, um resumo do que deve acontecer do começo ao fim e uma apresentação sobre você - o que você faz? O que você busca com a escrita? Já publicou antes? Quais suas redes sociais? Já enviou tudo isso? Oba! Então agora é só esperar e torcer.'})
-			bot.inlineButton('Catarse', {url: "https://www.catarse.me/mafagafo"})
-        ]
-		[
-			bot.inlineButton('Retornar', {msg, "/info"})
-		]
+	    bot.inlineButton('Catarse', {url: "https://www.catarse.me/mafagafo"})
+        ],
+	[
+	    bot.inlineButton('Retornar', {msg, "/info"})
+	]
     ]);
 
 	};
