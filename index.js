87a3b2f68709434dabc1e74c4312183a7c20b137
ArpMarchetto <51139879+ArpMarchetto@users.noreply.github.com>
2019-05-31 10:48:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var TelegramBot = require( 'node-telegram-bot-api' ),
     app     = express(),
     schedule = require('node-schedule');
 
-var TOKEN = process.env.TELEGRAM_API;
+var TOKEN = process.env.TELEGRAM_API; //Fazer TELEGRAM_API como uma VAR no Heroku.
 var rule = new schedule.RecurrenceRule();
 var bot = new TelegramBot( TOKEN, { polling: true } );
 
@@ -17,69 +17,33 @@ app.get('/', function(request, response) {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
 
-
 bot.on('message', (msg) => {     //O comando acima diz o que o bot faz quando recebe mensagens
 	console.log(msg,msg);
-	
-	var tchaus = ["falou", "flw", "falow", "tchau", "adeus"];
-	var gots = ["daenerys", " got", "game of thrones", "jon snow"];
-    var respostasGot = ['Amo a Daenerys, traçava ela todinha','Eu só gosto de Game of Thrones pq os nórdicos são muito macho. Dá até orgulho de ver uma geração dessa',
-			    'Casa Lannister é coisa de viado, vocês sabem, né?','Game of Thrones acabou do jeitinho que tinha que acabar', 
-				'Adorei esse final','Eu gosto da série da HBO pq tem tudo que um homem pode gostar: violência e peitos'];         
+	//var answers = 
+	//var random = Math.floor(Math.random() * answers.length);			
 	var x = Math.random()*100+1;
-	var randomGot = Math.floor(Math.random() * respostasGot.length);
-	var homofobia = ['Homofobia não é crime, é bom senso', 'odiar gays não é preconceito, é inevitável', 
-	'eu não sou homofóbico. Homofobia é crime e cadeia é coisa de viado', 'Eu odeio a palavra homofobia. Não é uma fobia. Porque eu teria medo de viado?', 'Deus fez os gays com o único propósito de queima-los.',
-	'E eu lá tenho medo de viado, caraio?','Eu nem sou homofóbico. Inclusive, tenho alguns amigos gays','Não precisa ficar contando para todo mundo que você é gay','Não tenho nada contra, só não curto ver dois marmanjos se beijando'];
-    var randomGay = Math.floor(Math.random() * homofobia.length);
 	
-	if (msg.text.toLowerCase() == 'oi' || msg.text.toLowerCase() == 'olá' || msg.text.toLowerCase() == 'e aí' || msg.text.toLowerCase() == 'ola'){ //MENSAGENS DE OI
-		if (x <50) {bot.sendMessage(msg.chat.id,"Fala meu leitão véio")}
-		if (x >=50) {bot.sendMessage(msg.chat.id, "Coé, chapa")}
-		return;
-	}
-	if (msg.text.toLowerCase() == 'fui' || msg.text.toLowerCase() == 'adeus' || msg.text.toLowerCase() == 'tchau' || msg.text.toLowerCase() == 'falou'){ //MENSAGENS DE TCHAU
-		if (x <50) {bot.sendMessage(msg.chat.id,"Até a próxima, pela saco")}
-		if (x >=50) {bot.sendMessage(msg.chat.id, "Vá curtir a praia de Copacabana, manin. Sem Kaô")}
-		return;
-	}
-	for (var i = 0; i < gots.length; i++){ //MENSAGENS DE GOT
-        if (msg.text.toString().toLowerCase().includes(gots[i])){
-		bot.sendMessage(msg.chat.id, respostasGot[randomGot])
-		return;	
-	   }
-    }
-	if(msg.from.id=='91863978'){ //QUANDO O IGOR RESPONDE
-		if(x < 10){bot.sendMessage(msg.chat.id, 'Ih, lá vem a poc...')}
-		if(x > 90){bot.sendMessage(msg.chat.id, 'Igor, manda umas foto de peito e churrasco aí pra mim')}
-        return;
-	}
-	if (msg.text.toLowerCase().includes('homofobia') || msg.text.toLowerCase().includes('homofóbico') || msg.text.toLowerCase().includes('homofobico')){ //HOMOFOBIA
-		bot.sendMessage(msg.chat.id, homofobia[randomGay])
-		return;	
-	}
-	if (msg.text.toLowerCase().includes('deivison') || msg.text.toLowerCase().includes('foucault')){ //DEIVISON
-		bot.sendMessage(msg.chat.id,"UMA BICHONA, NÉ")
-		return;	
-	}
+	if (msg.text.toLowerCase() == 'Mafagafo')){ //OPÇÕES - MAFAGAFO
+	bot.sendMessage(msg.chat.id, "A Revista Mafagafo é uma publicação digital de contos e noveletas de fantasia e ficção científica. Mais do que isso, a Mafagafo foi criada com foco no processo editorial. O material enviado para submissão é a matéria-prima e a revista publicada é o produto final. Nossa proposta é trabalhar no que fica entre um e outro, o processo editorial sério, o preparo cuidadoso do texto através do trabalho em equipe.")
+    let replyMarkup = bot.inlineKeyboard([
+        [
+            bot.inlineButton('Site e edições', {url: 'https://mafagaforevista.com.br/'}),
+            bot.inlineButton('Twitter', {url: "https://twitter.com/mafagaforevista"})
+        ], 
+		[
+            bot.inlineButton('Quero Publicar', {msg: 'Antes de tudo, é bom avisar que a Mafagafo publica contos e noveletas, o que significa que sua história precisa ter de 7,5 a 17,5 mil palavras. É o que tem a sua história? Ótimo! Então o próximo passo é verificar as datas e preparar o envio. Nessa etapa, você vai enviar o começo da sua hitória, separando um trecho entre 2 e 3 mil palavras. Você também vai precisar enviar a estimativa do número de palavras no texto final, o conceito bem resumido da sua história, um resumo do que deve acontecer do começo ao fim e uma apresentação sobre você - o que você faz? O que você busca com a escrita? Já publicou antes? Quais suas redes sociais? Já enviou tudo isso? Oba! Então agora é só esperar e torcer.'})
+			bot.inlineButton('Catarse', {url: "https://www.catarse.me/mafagafo"})
+        ]
+		[
+			bot.inlineButton('Retornar', {msg, "/info"})
+		]
+    ]);
+
+	};
+	
 });
+	
 
-//rule.second = 25;
-	//var intMail = schedule.scheduleJob(rule, function(){
-    //var answers = ["Carai, eu curto mesmo é uma brotheragem no banheiro", "Cês já fizeram um banheirão na estação? Só é zoado quando um zé ruela entra para atrapalhar"];
-    //var random1 = Math.floor(Math.random() * answers.length);
-   // var randomThing = answers[random1];
-   // bot.sendMessage("-1001259017807",randomThing);
-//});
-
-
-var sabedoria = function(msg, match){
-    var imagensHomofobicas = ['https://i.ytimg.com/vi/tBl4InwUhBI/hqdefault.jpg','https://www.gaystarnews.com/wp-content/uploads/2018/06/MILLIE-BOBBY-BROWN-STRANGER-THINGS-TWITTER.jpg','https://www.thewrap.com/wp-content/uploads/2018/06/Screen-Shot-2018-06-13-at-2.00.30-PM.png','https://www.pophatesflops.com/applications/core/interface/imageproxy/imageproxy.php?img=https://pbs.twimg.com/media/DfmYuSeUEAM_xwQ.jpg','https://i.kym-cdn.com/entries/icons/original/000/026/421/millie.jpg'];
-    bot.sendPhoto(msg.chat.id, imagensHomofobicas[Math.floor(Math.random() * imagensHomofobicas.length)]);
-    console.log(imagensHomofobicas[Math.floor(Math.random() * imagensHomofobicas.length)]);
-}
-
-//
 var informacoes = function(msg, match){
      bot.sendMessage(msg.chat.id, "Você quer receber mais informações sobre a Mafagafo? Clique em uma das opções abaixo: ", {
          "reply_markup": {
@@ -89,25 +53,13 @@ var informacoes = function(msg, match){
              ]
             }
     })
-	if (msg.text.toLowerCase().includes('Mafagafo')){ //OPÇÕES - MAFAGAFO
-		bot.remove_keyboard
-		bot.sendMessage(msg.chat.id, "A Mafagafo é uma revista que publica contos e noveletas de fantasia e ficção científica - e já possui dois filhotinhos, a Faísca e o Pio", {
-			"reply_markup": {
-			"inline_keyboard": [
-			["Site"], ["Twitter"], ["Quero publicar"], ["Última edição"], ["Retornar"]
-			]
-			}
-	 	})
-	};
 };
 
 
 
 var start = function (msg, match){
-	bot.sendMessage(msg.chat.id, "Olá, tudo bem? Eu sou o Mafagabot - um tio postiço dos mafagafinhos");
+	bot.sendMessage(msg.chat.id, "Olá, tudo bem? Eu sou o Mafagabot - um tio postiço dos mafagafinhos. Para receber informações, digite '/info'.")
 };
 
 bot.onText( /\/start/, start);
 bot.onText ( /\/info/, informacoes);
-//
-bot.onText( /\/sabedoria/, sabedoria);
